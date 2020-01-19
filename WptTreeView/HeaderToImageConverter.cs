faa533c5d0c26bf8707e4058ccbf603d7e292063
wojdam <wojdam@email.cz>
2020-01-20 00:06:06 +0100

    Advanced binding.

---
+++ WptTreeView/HeaderToImageConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace WptTreeView
+{
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var path = (string)value;
+
+            if (string.IsNullOrEmpty(path))
+                return null;
+
+            var image = "file-regular.png";
+
+            var directoryName = Path.GetFileName(path);
+
+            if (string.IsNullOrEmpty(directoryName))
+                image = "hdd-regular.png";
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+                image = "folder-regular.png";
+
+            return new BitmapImage(new Uri($"pack://application:,,,/Images/{image}"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WptTreeView/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WptTreeView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach(var drive in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem();
+
+                item.Header = drive;
+                item.Tag = drive;
+
+                item.Items.Add(null);
+
+                item.Expanded += Folder_Expanded;
+
+                this.FolderView.Items.Add(item);
+            }
+        }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = sender as TreeViewItem;
+
+            var fullPath = (string)item.Tag;
+
+            if (item.Items.Count != 1 || item.Items[0] != null)
+                return;
+
+            item.Items.Clear();
+
+            string[] dirs;
+            string[] files;
+
+            try
+            {
+                files = Directory.GetFiles(fullPath);
+
+                dirs = Directory.GetDirectories(fullPath);
+            }
+            catch(UnauthorizedAccessException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            foreach (var fx in dirs)
+            {
+                var directoryName = Path.GetFileName(fx);
+
+                var subItem = new TreeViewItem()
+                {
+                    Header = directoryName,
+
+                    Tag = fx
+                };
+
+                subItem.Items.Add(null);
+
+                subItem.Expanded += Folder_Expanded;
+
+                item.Items.Add(subItem);
+            }
+
+            foreach (var fx in files)
+            {
+                var directoryName = Path.GetFileName(fx);
+
+                var subItem = new TreeViewItem()
+                {
+                    Header = directoryName,
+
+                    Tag = fx
+                };
+
+                subItem.Expanded += Folder_Expanded;
+
+                item.Items.Add(subItem);
+            }
+
+
+
+        }
+    }
+}
