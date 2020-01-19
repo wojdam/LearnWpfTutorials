136275aa0e84657fd9a197763abb82a8429d8009
wojdam <wojdam@email.cz>
2020-01-19 13:02:45 +0100

    Very basic layout wpf.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,78 @@ namespace LearnWpfTutorials
         {
             InitializeComponent();
         }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Description text: {this.DescriptionText.Text}");
+
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResetWorkCentersCheckBoxes();
+        }
+
+        /// <summary>
+        /// Just reset checkboxes connected to the work centers.
+        /// </summary>
+        private void ResetWorkCentersCheckBoxes()
+        {
+            this.WeldCheckBox.IsChecked = false;
+            this.AssemblyCheckBox.IsChecked = false;
+            this.LaserCheckBox.IsChecked = false;
+            this.PurchaseCheckBox.IsChecked = false;
+            this.PlasmaCheckBox.IsChecked = false;
+
+            this.LatheCheckBox.IsChecked = false;
+            this.DrillCheckBox.IsChecked = false;
+            this.FoldCheckBox.IsChecked = false;
+            this.RollCheckBox.IsChecked = false;
+            this.SawCheckBox.IsChecked = false;
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            var checkBox = sender as CheckBox;
+
+            this.LengthTextBox.Text += checkBox.Content;
+        }
+
+        /// <summary>
+        /// Drop down finish drop down.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FinishDropdown_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.NoteTextBox == null)
+                return;
+
+            var combo = sender as ComboBox;
+
+            var item = combo.SelectedValue as ComboBoxItem;
+
+            this.NoteTextBox.Text = (string) item.Content;
+        }
+
+        /// <summary>
+        /// This event is triggered when the MainWindow is loaded.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.FinishDropdown_SelectionChanged(this.FinishDropdown, null);
+        }
+
+        /// <summary>
+        /// Supplier name text changed event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SupplierNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.MassTextBox.Text = this.SupplierNameTextBox.Text;
+        }
     }
 }
